55939fe8d2b928ba7107cf3f628a13404c96262c
Mketia <k.mukakalis1@alustudent.com>
2026-02-22 17:35:12 +0200

    feat: update App layout for centered quiz card

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Quiz from './Quiz'
+
+function App() {
+  return (
+    <div className='min-h-screen w-full flex items-center justify-center bg-[#f4f7fa]'>
+      <Quiz />
+    </div>
+  )
+}
+
+export default App
