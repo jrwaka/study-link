b79aef2d0dd7ea01fc53f49ae0a5672b275a6db9
jrwaka <j.rwaka@alustudent.com>
2026-03-23 19:16:05 +0200

    feat: add Terraform configuration and Ansible setup for Azure deployment

---
+++ terraform/outputs.tf
@@ -0,0 +1,27 @@
+# Resource Group
+
+output "resource_group_name" {
+  description = "Name of the Azure resource group"
+  value = azurerm_resource_group.main.name
+}
+
+# Virtual Machine
+
+output "app_vm_private_ip" {
+  description = "Private IP of the application VM"
+  value = azurerm_network_interface.app_nic.private_ip_address
+}
+
+# Bastion Host
+
+output "bastion_public_ip" {
+  description = "Public IP of the bastion host"
+  value = azurerm_public_ip.bastion_pip.ip_address
+}
+
+# Container Registry
+
+output "acr_login_server" {
+  description = "ACR login server URL — used to push/pull Docker images"
+  value = azurerm_container_registry.acr.login_server
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,54 @@
+# Subscription ID, location, and project name
+
+variable "subscription_id" {
+  description = "Your Azure subscription ID"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where all resources will be created"
+  type        = string
+  default     = "East US"
+}
+
+variable "project_name" {
+  description = "Name prefix used for all resources (keep it short, lowercase)"
+  type        = string
+  default     = "studylink"
+}
+
+# Virtual Machine
+
+variable "vm_size" {
+  description = "Size of the main application VM"
+  type        = string
+  default     = "Standard_B1s"
+}
+
+variable "admin_username" {
+  description = "Admin username for SSH access to the VMs"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "admin_password" {
+  description = "Admin password for the VMs"
+  type        = string
+  sensitive   = true
+}
+
+# Bastion Host
+
+variable "bastion_vm_size" {
+  description = "Size of the bastion host VM"
+  type        = string
+  default     = "Standard_B1s"
+}
+
+# Container Registry
+
+variable "acr_sku" {
+  description = "Azure Container Registry pricing tier"
+  type        = string
+  default     = "Basic"
+}
